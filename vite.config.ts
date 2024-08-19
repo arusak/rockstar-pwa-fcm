4324be6016829ee7640d167208fae49189c1f740
Anton Rusak <anton.rusak@perfectart.com>
2024-08-19 18:41:30 +0300

    precache index

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import pathsPlugin from 'vite-tsconfig-paths';
 import checkerPlugin from 'vite-plugin-checker';
-import { VitePWA } from 'vite-plugin-pwa';
-import { readFileSync } from 'fs';
+import {VitePWA} from 'vite-plugin-pwa';
+import {readFileSync} from 'fs';
 import {readableCssModules} from 'vite-plugin-readable-css-modules';
 
-export default defineConfig(({ mode }) => {
-  const { SSL_PRIVATE_KEY_PATH, SSL_PUBLIC_KEY_PATH } = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({mode}) => {
+  const {SSL_PRIVATE_KEY_PATH, SSL_PUBLIC_KEY_PATH} = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [
       react(),
@@ -33,13 +33,13 @@ export default defineConfig(({ mode }) => {
               src: 'icon192.png',
               sizes: '192x192',
               type: 'image/png',
-              purpose: 'maskable any'
+              purpose: 'maskable any',
             },
             {
               src: 'icon512.png',
               sizes: '512x512',
               type: 'image/png',
-              purpose: 'maskable any'
+              purpose: 'maskable any',
             },
             {
               src: 'icon192.png',
@@ -58,11 +58,17 @@ export default defineConfig(({ mode }) => {
           enabled: true,
           type: 'module',
         },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        },
       }),
-      readableCssModules()
+      readableCssModules(),
     ],
     define: {
-      "import.meta.env.VITE_BUILD": JSON.stringify(new Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'short'}).format(new Date()))
+      "import.meta.env.VITE_BUILD": JSON.stringify(new Intl.DateTimeFormat('en-GB', {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      }).format(new Date())),
     },
     css: {
       devSourcemap: true,
@@ -79,7 +85,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'build',
-      sourcemap: 'inline'
+      sourcemap: 'inline',
     },
     preview: {
       port: 8080,
